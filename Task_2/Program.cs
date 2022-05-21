699fa1d354c6597cdb978c21fd81f0fe279b5ecb
MaximBabenkov <lostbass@mail.ru>
2022-05-21 23:12:08 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму чисел от 1 до a
+Console.Write("Enter your number: ");
+int a = int.Parse(Console.ReadLine()??"0");
+int sum = 0;
+for (int i=1;i<=a;i++)
+{
+    sum = sum+i;
+}
+Console.Write($"The sum of numbers from 1 to {a}: {sum}");
