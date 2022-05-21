666ab028b79511bb992849f337ece5e5e1050975
MaximBabenkov <lostbass@mail.ru>
2022-05-21 23:25:35 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+Console.Write("Enter your number: ");
+int a = int.Parse(Console.ReadLine()??"0");
+int i = 0;
+while (a>0)
+{
+    a=a/10;
+    i++;
+}
+Console.Write($"The quantity of digits: {i}");
