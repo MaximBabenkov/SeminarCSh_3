0bbd55dd0551e8bf89690220b24e4e1a6c8224e3
MaximBabenkov <lostbass@mail.ru>
2022-05-21 23:40:49 +0300

    Task_5

---
+++ Task_5/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+int[]array=new int[8];
+for(int i=0;i<array.Length;i++)
+{
+    array[i]=new Random().Next(1,20);
+    Console.Write($"{array[i]} ");
+}
