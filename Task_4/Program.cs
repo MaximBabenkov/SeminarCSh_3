484dba7a11f1c68a262ef2e4b002d103ebff3352
MaximBabenkov <lostbass@mail.ru>
2022-05-21 23:32:33 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти произведение чисел от 1 до a
+Console.Write("Enter your number: ");
+int a = int.Parse(Console.ReadLine()??"0");
+int prod = 1;
+for (int i=1;i<=a;i++)
+{
+    prod = prod*i;    
+}
+Console.Write($"The product of numbers from 1 to {a}: {prod}");
