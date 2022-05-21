1ad6cfb7caf66618fd606c477392fc011d34a289
MaximBabenkov <lostbass@mail.ru>
2022-05-21 23:02:42 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать таблицу квадратов чисел от 1 до n
+Console.Write("Enter your number: ");
+int n = int.Parse(Console.ReadLine()??"0");
+for (int i=1;i<=n;i++)
+{
+    Console.Write($"{i*i} ");
+}
+
